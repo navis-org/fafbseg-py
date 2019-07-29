3896625256886bbc3e436c8ed7cb18a6a86624db
Philipp Schlegel <observing@web.de>
2019-07-29 15:37:36 +0100

    optimize find_fragments: - use treenode table instead of full neurons for initial search - fix min_node_overlap to correctly count shared number of nodes in the   same segments(s) - attach `.overlap_score` as neuron attributes

--- fafbseg/search.py
+++ fafbseg/search.py
@@ -24,7 +24,7 @@ from tqdm import tqdm
 
 
 @never_cache
-def find_fragments(x, remote_instance, min_overlap=3, min_nodes=1):
+def find_fragments(x, remote_instance, min_node_overlap=3, min_nodes=1):
     """Find fragments constituting a neuron in another CatmaidInstance,
     e.g. manual tracings for an autoseg neuron.
 
@@ -33,7 +33,7 @@ def find_fragments(x, remote_instance, min_overlap=3, min_nodes=1):
         2. Collect segmentation IDs for the input neuron and all potentially
            overlapping fragments using the brainmaps API.
         3. Return fragments that overlap with at least ``min_overlap`` nodes
-           in the same segmentation ID with the input neuron.
+           with input neuron.
 
 
     Parameters
@@ -42,33 +42,34 @@ def find_fragments(x, remote_instance, min_overlap=3, min_nodes=1):
                         Neuron to collect fragments for.
     remote_instance :   pymaid.CatmaidInstance
                         Catmaid instance in which to search for fragments.
-    min_overlap :       int, optional
-                        Minimal overlap between `x` and a fragment. If the
-                        fragment has less nodes than `min_overlap`, the
-                        threshold will be lowered accordingly.
+    min_node_overlap :  int, optional
+                        Minimal overlap between `x` and a fragment in nodes. If
+                        the fragment has less total nodes than `min_overlap`,
+                        the threshold will be lowered to:
+                        ``min_overlap = min(min_overlap, fragment.n_nodes)
     min_nodes :         int, optional
-                        Minimum node count for returned neurons. For each
-                        fragment the threshold is::
-
-                            min(min_nodes, total nodes of fragment)
+                        Minimum node count for returned neurons.
 
     Return
     ------
     pymaid.CatmaidNeuronList
                         CatmaidNeurons of the overlapping fragments.
+    list
+                        List with number of nodes that each fragment overlaps
+                        with input neuron.
 
     Examples
     --------
     >>> import pymaid
-    >>> auto = pymaid.CatmaidInstance('http://your-server/auto', 'http_user', 'http_pw', 'api_token')
-    >>> manual = pymaid.CatmaidInstance('http://your-server/manual', 'http_user', 'http_pw', 'api_token')
+    >>> rm1 = pymaid.CatmaidInstance('SERVER_URL', 'HTTP_USER', 'HTTP_PW', 'API_TOKEN')
+    >>> rm2 = pymaid.CatmaidInstance('SERVER_URL', 'HTTP_USER', 'HTTP_PW', 'API_TOKEN')
 
     >>> import brainmappy as bm
     >>> flow = bm.acquire_credentials('client_secret.json', make_global=True)
     >>> bm.set_global_volume('some_volume_id')
 
-    >>> x_auto = pymaid.get_neuron(204064470, remote_instance=auto)
-    >>> x_man = find_fragments(x_auto, remote_instance=manual)
+    >>> x = pymaid.get_neuron(204064470, remote_instance=rm1)
+    >>> frags_of_x = find_fragments(x_auto, remote_instance=rm2)
 
     """
     if not isinstance(x, pymaid.CatmaidNeuron):
@@ -93,72 +94,73 @@ def find_fragments(x, remote_instance, min_overlap=3, min_nodes=1):
     resp = remote_instance.fetch(urls, desc='Searching for overlapping neurons')
     skids = set([s for l in resp for s in l])
 
-    # Get these candidates
-    cand = pymaid.get_neurons(skids, remote_instance=remote_instance)
+    # Get nodes for these candidates
+    tn_table = pymaid.get_treenode_table(skids,
+                                         include_details=False,
+                                         convert_ts=False,
+                                         remote_instance=remote_instance)
+    # Keep track of total node counts
+    node_counts = tn_table.groupby('skeleton_id').treenode_id.count().to_dict()
 
     # Get segment IDs for the input neuron
-    x_segs = bm.get_seg_at_location(x.nodes[['x', 'y', 'z']].values)
+    x.nodes['seg_id'] = bm.get_seg_at_location(x.nodes[['x', 'y', 'z']].values)
 
-    # Remove segment ID "0" which is glia (I believe) and count occurrences
-    x_segs = np.array(x_segs)
-    x_uni, x_count = np.unique(x_segs[x_segs != "0"], return_counts=True)
+    # Count segment IDs
+    x_seg_counts = x.nodes.groupby('seg_id').treenode_id.count().reset_index(drop=False)
+    x_seg_counts.columns = ['seg_id', 'counts']
 
-    # Discard segs with lower than min_overlap counts
-    x_uni = x_uni[x_count >= min_overlap]
+    # Remove seg IDs 0
+    x_seg_counts = x_seg_counts[x_seg_counts.seg_id != '0']
 
+    # Generate KDTree for nearest neighbor calculations
     tree = pymaid.neuron2KDTree(x)
 
-    # Go over each candidate and collect positions to query
-    nodes = []
-    for c in cand:
-        # Find points in this neuron that are actually close to the input neuron
-        dist, ix = tree.query(c.nodes[['x', 'y', 'z']].values,
-                              distance_upper_bound=2500)
-
-        # If the neuron is smaller than min_overlap, lower the threshold
-        this_min_ol = min(min_overlap, c.nodes.shape[0])
-
-        # Skip prematurely if there is no way for us to get enough overlap
-        if sum(dist <= 2500) < this_min_ol:
-            continue
-
-        this_nodes = c.nodes.loc[dist <= 2500,
-                                 ['treenode_id', 'x', 'y', 'z']]
-        this_nodes['skeleton_id'] = c.skeleton_id
-
-        nodes.append(this_nodes)
-
-    # Turn this into a single stack
-    nodes = pd.concat(nodes, axis=0)
+    # Now remove nodes that aren't even close to our input neuron
+    dist, ix = tree.query(tn_table[['x', 'y', 'z']].values,
+                          distance_upper_bound=2500)
+    tn_table = tn_table.loc[dist <= 2500]
 
     # Add segment IDs
-    nodes['seg_id'] = bm.get_seg_at_location(nodes[['x', 'y', 'z']].values)
+    tn_table['seg_id'] = bm.get_seg_at_location(tn_table[['x', 'y', 'z']].values)
 
-    # Go over each candidate and if there is enough overlap
+    # Now group by neuron and by segment
+    seg_counts = tn_table.groupby(['skeleton_id', 'seg_id']).treenode_id.count().reset_index(drop=False)
+    # Rename columns
+    seg_counts.columns = ['skeleton_id', 'seg_id', 'counts']
+
+    # Remove seg IDs 0
+    seg_counts = seg_counts[seg_counts.seg_id != '0']
+
+    # Remove segments IDs that are not overlapping with input neuron
+    seg_counts = seg_counts[np.isin(seg_counts.seg_id.values,
+                                    x_seg_counts.seg_id.values)]
+
+    # Now go over each candidate and see if there is enough overlap
     ol = []
-    for c in cand:
-        # Subset to nodes that we queried
-        this_nodes = nodes[nodes.skeleton_id == c.skeleton_id]
-
-        # Skip prematurely if there are no nodes to check
-        if this_nodes.empty:
-            continue
+    scores = []
+    for s in seg_counts.skeleton_id.unique():
+        # Subset to nodes of this neurons
+        this_counts = seg_counts[seg_counts.skeleton_id == s]
 
         # If the neuron is smaller than min_overlap, lower the threshold
-        this_min_ol = min(min_overlap, c.nodes.shape[0])
+        this_min_ol = min(min_node_overlap, node_counts[s])
 
-        # Count unique segments (discarding "0" which is glia)
-        c_seg = np.array(this_nodes['seg_id'].values)
-        c_uni, c_count = np.unique(c_seg[c_seg != "0"], return_counts=True)
+        # Sum up counts for both input neuron and this candidate
+        c_count = this_counts.counts.sum()
+        x_count = x_seg_counts[x_seg_counts.seg_id.isin(this_counts.seg_id.values)].counts.sum()        
 
-        # Find segment IDs that occur more than min_overlap and are also present
-        # in input neuron
-        c_ol = (c_count >= this_min_ol) & np.isin(c_uni, x_uni)
-        # NOTE: this is currently filtering to min_overlap per segment
-        #       -> we should make this per fragment
+        # If there is enough overlap, keep this neuron
+        # and add score as `overlap_score`
+        if (c_count >= this_min_ol) and (x_count >= this_min_ol):
+            # The score is the minimal overlap
+            scores.append(min(c_count, x_count))
+            ol.append(s)
 
-        # If there is overlap, keep this neuron
-        if any(c_ol):
-            ol.append(c)
+    if ol:
+        ol = pymaid.get_neurons(ol, remote_instance=remote_instance)
+        for n, s in zip(ol, scores):
+            n.overlap_score = s
+    else:
+        ol = pymaid.CatmaidNeuronList([])
 
-    return pymaid.CatmaidNeuronList(ol)
+    return ol
