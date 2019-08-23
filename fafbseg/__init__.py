5e91e7b5c6e814cca28f5ef759b85cb33c877e05
Philipp Schlegel <observing@web.de>
2019-08-23 14:14:06 +0100

    bump version 0.1.5 -> 0.1.6

--- fafbseg/__init__.py
+++ fafbseg/__init__.py
@@ -2,4 +2,4 @@ from .merge import find_missed_branches, merge_neuron
 from .search import (segments_to_skids, segments_to_neuron,
                      neuron_to_segments, find_autoseg_fragments, find_fragments)
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
