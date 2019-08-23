a033e1d4a39b0bac8925ab96ed9cfff1e8255dd8
Philipp Schlegel <observing@web.de>
2019-08-23 09:46:20 +0100

    add missing function to top level import

--- fafbseg/__init__.py
+++ fafbseg/__init__.py
@@ -1,5 +1,5 @@
 from .merge import find_missed_branches, merge_neuron
 from .search import (segments_to_skids, segments_to_neuron,
-                     find_autoseg_fragments, find_fragments)
+                     neuron_to_segments, find_autoseg_fragments, find_fragments)
 
 __version__ = "0.1.4"
