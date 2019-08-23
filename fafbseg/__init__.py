91030b0ec4420d1941a3be9a26e4af1c2e3ec86a
Philipp Schlegel <observing@web.de>
2019-08-23 09:41:27 +0100

    explicit import at top level

--- fafbseg/__init__.py
+++ fafbseg/__init__.py
@@ -1,4 +1,5 @@
-from .merge import *
-from .search import *
+from .merge import find_missed_branches, merge_neuron
+from .search import (segments_to_skids, segments_to_neuron,
+                     find_autoseg_fragments, find_fragments)
 
 __version__ = "0.1.4"
