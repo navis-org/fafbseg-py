5313ef0941e07e6e5fb1327d11d7828c1e34ec51
Philipp Schlegel <observing@web.de>
2021-04-16 17:40:13 +0100

    update l2_skeleton()

--- fafbseg/flywire/l2.py
+++ fafbseg/flywire/l2.py
@@ -21,9 +21,9 @@ Heavily borrows from code from Casey Schneider-Mizell's "pcg_skel"
 """
 
 import navis
-
 import fastremap
 
+import networkx as nx
 import numpy as np
 import skeletor as sk
 import trimesh as tm
@@ -32,12 +32,13 @@ from annotationframeworkclient import FrameworkClient
 from concurrent.futures import ThreadPoolExecutor
 
 from .utils import parse_volume
+from .. import spine
 
 __all__ = ['l2_skeleton']
 
 
-def l2_skeleton(root_id, refine=False, drop_missing=True, threads=10,
-                progress=True, dataset='production'):
+def l2_skeleton(root_id, refine=False, drop_missing=True,
+                threads=10, progress=True, dataset='production', **kwargs):
     """Generate skeleton from L2 graph.
 
     Parameters
@@ -58,23 +59,30 @@ def l2_skeleton(root_id, refine=False, drop_missing=True, threads=10,
     threads :           int
                         How many parallel threads to use for fetching the
                         chunk meshes. Reduce the number if you run into
-                        ``HTTPErrors``.
+                        ``HTTPErrors``. Only relevant if `use_flycache=False`.
     progress :          bool
                         Whether to show a progress bar.
 
-
-    Return
-    ------
+    Returns
+    -------
     skeleton :          navis.TreeNeuron
-                        The extraced skeleton.
+                        The extracted skeleton.
 
     Examples
     --------
+    >>> from fafbseg import flywire
     >>> n = flywire.l2_skeleton(720575940614131061)
 
     """
     # TODO:
     # - drop duplicate nodes in unrefined skeleton
+    # - use L2 graph to find soma: highest degree is typically the soma
+
+    use_flycache = kwargs.get('use_flycache', False)
+
+    if refine and use_flycache and dataset != 'production':
+        raise ValueError('Unable to use fly cache to fetch L2 centroids for '
+                         'sandbox dataset. Please set `use_flycache=False`.')
 
     if navis.utils.is_iterable(root_id):
         nl = []
@@ -115,8 +123,12 @@ def l2_skeleton(root_id, refine=False, drop_missing=True, threads=10,
 
     # This turns the graph into a hierarchal tree by removing cycles and
     # ensuring all edges point towards a root
-    G = sk.skeletonizers.edges_to_graph(eg_arr_rm)
-    swc = sk.skeletonizers.make_swc(G, coords=coords)
+    if sk.__version_vector__[0] < 1:
+        G = sk.skeletonizers.edges_to_graph(eg_arr_rm)
+        swc = sk.skeletonizers.make_swc(G, coords=coords)
+    else:
+        G = sk.skeletonize.utils.edges_to_graph(eg_arr_rm)
+        swc = sk.skeletonize.utils.make_swc(G, coords=coords, reindex=False)
 
     # Convert to Euclidian space
     # Dimension of a single chunk
@@ -127,25 +139,17 @@ def l2_skeleton(root_id, refine=False, drop_missing=True, threads=10,
     swc[['x', 'y', 'z']] = chunks_to_nm(xyz, vol) + ch_dims / 2
 
     if refine:
-        with ThreadPoolExecutor(max_workers=threads) as pool:
-            futures = [pool.submit(vol.mesh.get, i,
-                                   allow_missing=True,
-                                   deduplicate_chunk_boundaries=False) for i in l2_ids]
+        if use_flycache:
+            centroids = spine.flycache.get_L2_centroids(l2_ids,
+                                                        progress=progress)
 
-            res = [f.result() for f in navis.config.tqdm(futures,
-                                                         disable=not progress,
-                                                         leave=False,
-                                                         desc='Loading meshes')]
+            # Drop missing (i.e. [0,0,0]) meshes
+            centroids = {k: v for k, v in centroids.items() if v != [0, 0, 0]}
+        else:
+            # Get the centroids
+            centroids = get_L2_centroids(l2_ids, vol, threads=threads, progress=progress)
 
-        # Unpack results
-        meshes = {k: v for d in res for k, v in d.items()}
-
-        # For each mesh find the center of mass and move the corresponding point
-        new_co = {}
-        for k, m in meshes.items():
-            m = tm.Trimesh(m.vertices, m.faces)
-            # Do NOT use center_mass here -> garbage if not non-watertight
-            new_co[l2dict[k]] = m.centroid
+        new_co = {l2dict[k]: v for k, v in centroids.items()}
 
         # Map refined coordinates onto the SWC
         has_new = swc.node_id.isin(new_co)
@@ -163,6 +167,97 @@ def l2_skeleton(root_id, refine=False, drop_missing=True, threads=10,
         tn = navis.TreeNeuron(swc, id=root_id, units='1 nm')
 
     return tn
+
+
+def get_L2_centroids(l2_ids, vol, threads=10, progress=True):
+    with ThreadPoolExecutor(max_workers=threads) as pool:
+        futures = [pool.submit(vol.mesh.get, i,
+                               allow_missing=True,
+                               deduplicate_chunk_boundaries=False) for i in l2_ids]
+
+        res = [f.result() for f in navis.config.tqdm(futures,
+                                                     disable=not progress,
+                                                     leave=False,
+                                                     desc='Loading meshes')]
+
+    # Unpack results
+    meshes = {k: v for d in res for k, v in d.items()}
+
+    # For each mesh find the center of mass and move the corresponding point
+    centroids = {}
+    for k, m in meshes.items():
+        m = tm.Trimesh(m.vertices, m.faces)
+        # Do NOT use center_mass here -> garbage if not non-watertight
+        centroids[k] = m.centroid
+
+    return centroids
+
+
+def l2_soma(x, dataset='production', progress=True):
+    """DOES NOT WORK. Use the L2 graph to guess the soma location.
+
+    In a nutshell: we use the connectedness (i.e. the degree) of L2 chunks to
+    guess a neurons soma. The idea was that L2 chunk with the highest degree is
+    in the center of the soma. Unfortunately, that only seems to work if the
+    soma is really really big.
+
+    Parameters
+    ----------
+    x  :                int | nx.Graph | lists thereof
+                        The neuron(s) for which to find somas. Can be either:
+                            - root ID (s)
+                            - networkx L2 graph(s)
+
+    Returns
+    -------
+    [x, y, z]
+
+    Examples
+    --------
+    >>> n = flywire.l2_soma(720575940614131061)
+
+    """
+    if navis.utils.is_iterable(x):
+        res = []
+        for id in navis.config.tqdm(x, desc='Finding somas',
+                                    disable=not progress, leave=False):
+            res.append(l2_soma(id, progress=False))
+        return res
+
+    # Get the cloudvolume
+    vol = parse_volume(dataset)
+
+    # Hard-coded datastack names
+    ds = {"production": "flywire_fafb_production",
+          "sandbox": "flywire_fafb_sandbox"}
+    # Note that the default server url is https://global.daf-apis.com/info/
+    client = FrameworkClient(ds.get(dataset, dataset))
+
+    if not isinstance(x, nx.Graph):
+        # Load the L2 graph for given root ID
+        # This is a (N,2) array of edges
+        l2_eg = np.array(client.chunkedgraph.level2_chunk_graph(x))
+
+        # Drop duplicate edges
+        l2_eg = np.unique(np.sort(l2_eg, axis=1), axis=0)
+
+        G = nx.Graph()
+        G.add_edges_from(l2_eg)
+    else:
+        G = x
+
+    # Get degrees from the graph
+    degrees = np.array(list(G.degree))
+    l2_ids = degrees[:, 0]
+    l2_degrees = degrees[:, 1]
+
+    # Find the node with highest degree
+    mx_deg = l2_ids[np.argmax(l2_degrees)]
+
+    # Get it's centroid
+    centroid = get_L2_centroids([mx_deg], vol, threads=1, progress=False)
+
+    return centroid.get(mx_deg, 'NA')
 
 
 def chunks_to_nm(xyz_ch, vol, voxel_resolution=[4, 4, 40]):
