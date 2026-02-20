e190955d677d8ed17ee9cf4617eeefe9e78e6625
Philipp Schlegel <observing@web.de>
2026-02-20 13:28:28 +0000

    fix conftest.py

--- conftest.py
+++ conftest.py
@@ -1,17 +1,23 @@
 import os
+from pathlib import Path
 
 # This is to avoid some print statements in the code that are semi-random and
 # hence will make doctests fails. I tried adding this environment variable to
 # pytest.ini but that didn't work.
-os.environ['FAFBSEG_TESTING'] = 'TRUE'
+os.environ["FAFBSEG_TESTING"] = "TRUE"
 
-SKIP = ['conf.py']
+SKIP = ["conf.py"]
 
-def pytest_ignore_collect(path):
-    # Do not test the Blender module - for some reason that can't be imported
+
+def pytest_ignore_collect(collection_path: Path, config):
+    """Return True to prevent considering this path for collection.
+    This hook is consulted for all files and directories prior to calling
+    more specific hooks.
+    """
+    path = str(collection_path)
     if "blender" in str(path):
         return True
+
     for s in SKIP:
         if str(path).endswith(s):
             return True
-
